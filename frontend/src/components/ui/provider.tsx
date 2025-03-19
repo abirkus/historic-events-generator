142c9822a49c324301762b091fd533ab4225ec03
Andre Birkus <birkusandre@gmail.com>
2025-03-19 13:41:18 -0500

    front end changes

--- frontend/src/components/ui/provider.tsx
+++ frontend/src/components/ui/provider.tsx
@@ -3,7 +3,7 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode"
 
-export default function Provider(props: ColorModeProviderProps) {
+export function Provider(props: ColorModeProviderProps) {
 	return (
 		<ChakraProvider value={defaultSystem}>
 			<ColorModeProvider {...props} />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import { Provider } from "./components/ui/provider"
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider>
