7338db6df3f2f312ae12472462a64cc2fa102801
Andre Birkus <birkusandre@gmail.com>
2025-08-19 11:17:32 -0500

    updating FE for SEO and social media sharing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { configDefaults } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  server: {
+    port: 3000,
+    host: true,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          utils: ["axios"],
+        },
+      },
+    },
+  },
+  publicDir: "public",
   test: {
     ...configDefaults,
     environment: "jsdom",
